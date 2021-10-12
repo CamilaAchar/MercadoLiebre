26e0a6be743b0c11550a88dc5786b55ed33d9be1
CamilaAchar <achartrombacamila@gmail.com>
2021-10-11 21:37:49 -0300

    proyecto terminado

---
+++ app.js
@@ -0,0 +1,14 @@
+
+const express = require('express');
+const app = express();
+
+const path = require('path');
+const pathPublic = path.join(__dirname, './public');
+app.use(express.static(pathPublic));
+
+// app.listen(3050, console.log('Servidor OK - mercadoliebre - puerto 3050'));
+app.listen(process.env.PORT || 3050, function () { console.log('Servidor OK - mercadoliebre - puerto 3050')});
+app.get('/', (req,res) => {res.sendFile(path.join(__dirname, './views/home.html'))});
+app.get('/home', (req,res) => {res.sendFile(path.join(__dirname, './views/home.html'))});
+app.get('/register', (req,res) => {res.sendFile(path.join(__dirname, './views/register.html'))});
+app.get('/login', (req,res) => {res.sendFile(path.join(__dirname, './views/login.html'))});
