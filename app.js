3b616bf85e5fdc43b2e74692dc6dab15be0cfa2a
CamilaAchar <achartrombacamila@gmail.com>
2021-10-11 22:15:18 -0300

    agregue Procfile

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ const pathPublic = path.join(__dirname, './public');
 app.use(express.static(pathPublic));
 
 // app.listen(3050, console.log('Servidor OK - mercadoliebre - puerto 3050'));
-app.listen(process.env.PORT || 3050, function () { console.log('Servidor OK - mercadoliebre - puerto 3050')});
+App.listen (process.env.PORT || 3000, function () {
+    console.log ("Servidor corriendo en el puerto 3000")});
 app.get('/', (req,res) => {res.sendFile(path.join(__dirname, './views/home.html'))});
 app.get('/home', (req,res) => {res.sendFile(path.join(__dirname, './views/home.html'))});
 app.get('/register', (req,res) => {res.sendFile(path.join(__dirname, './views/register.html'))});
